4999e83531694b53a22dd52749ea56b46f83eb2d
iritin5321 <iritin@gmail.com>
2025-12-02 17:01:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -611,7 +611,7 @@ app.use(bot.webhookCallback(`/bot${BOT_TOKEN}`));
 const cron = require('node-cron');
 
 // Daily reminder at 10:00 server time
-cron.schedule('0 17 * * *', async () => {
+cron.schedule('10 17 * * *', async () => {
     console.log("Sending daily reminders...");
 
     const spreadsheetId = '1Sa4eOSmt4sxYq2ksmLqOGH3n4yod0lJmGJqW8ZXQgiE';
@@ -643,6 +643,7 @@ cron.schedule('0 17 * * *', async () => {
 process.once('SIGINT', () => bot.stop('SIGINT'));
 
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
 
 
 
